70ffbd0f1a65337483e3d47a2c7823f93d759880
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-13 14:34:51 -0500

    finished exercise 0

---
+++ exercises.py
@@ -0,0 +1,11 @@
+#Exercise 0
+#Lists of my favourite colours, age of me and my siblings, five coin flips, and three artists
+fav_colours = ['red','blue','green']
+ages = [29,30,32,33]
+coins = [True,False,False,True,True]
+fav_artists = ['The Offspring','Great Big Sea','Barenaked Ladies']
+#Dicts of three words & definitions, favourite movie names and their years, three cities and their pops, names of my siblings and their ages
+words = {'croze': 'groove in the staves of a cask', 'soliloquent': 'speaking in soliloquies; prone to giving soliloquies', 'recte': 'correctly; which should be'}
+fav_movies = {'BASEketball': 1998, 'Blade Runner 2049': 2017, 'Back to the Future': 1985}
+cities = {'Chicago': 2695598, 'Halifax': 403131, "St. John's": 108860}
+my_siblings = {'John': 33, 'Darrell': 32, 'Jeff': 30, 'Kim': 29}
