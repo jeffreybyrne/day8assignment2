90df8ec5bced92fcd1c57be13bb4a87a18658993
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-13 14:50:36 -0500

    finished exercise 3

--- exercises.py
+++ exercises.py
@@ -37,3 +37,18 @@ print(cities['Halifax'])
 # Print out a sentence about each item in the list of performing artists.
 for artist in fav_artists:
     print("I think {} is pretty neat".format(artist))
+
+#Exercise 3
+# Print out the first two performing artists in that list.
+print(fav_artists[0])
+print(fav_artists[1])
+# For each of your favourite movies, print out a sentence about when the movie was released.
+for movie, value in fav_movies.items():
+    print("{} came out in {}".format(movie,value))
+# Sort and reverse the list of ages of your family. Save it and print it to the screen.
+# See if you can sort and reverse the list on one line!
+ages.reverse()
+print(ages)
+# Add "Beauty and the Beast" movie to your dictionary of movies information, but with a twist: the movie was released both in 1991 and in 2017. Print it out.
+fav_movies["Beauty and the Beast"] = '1991,2017'
+print(fav_movies)
