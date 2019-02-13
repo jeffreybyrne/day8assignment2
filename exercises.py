92039c56dff5d4cc0d007d9204185f532019a403
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-13 15:06:49 -0500

    finished exercise 6

--- exercises.py
+++ exercises.py
@@ -86,3 +86,30 @@ for sibling, age in my_siblings.items():
 # Add two new colours to your list of favourite colours.
 fav_colours.append('fuschia')
 fav_colours.append('cerulean')
+
+#Exercise 6
+# Make a new dictionary that contains a list of movies for each year. Each list of movies should be a list. Below is some data you can use.
+#
+# 1999: The Matrix, Star Wars: Episode 1, The Mummy
+# 2009: Avatar, Star Trek, District 9
+# 2019: How to Train Your Dragon 3, Toy Story 4, Star Wars: Episode 9
+movies_by_year = {
+    1999: ['The Matrix', 'Star Wars: Episode 1', 'The Mummy'],
+    2009: ['Avatar', 'Star Trek', 'District 9'],
+    2019: ['How to Train Your Dragon 3', 'Toy Story 4', 'Star Wars: Episode 9']
+}
+# Make a new list that contains each row of the buttons on a phone. Each row should be a list.
+# The rows on a phone are: 1 2 3, 4 5 6, 7 8 9, * 0 #
+phone_nums = [[1,2,3],[4,5,6],['*',0,'#']]
+# Make a new list that contains information about three countries. Each country should be a dictionary that has a name, a continent, and whether or not it is an island.
+countries = [
+    {'name': 'Ireland',
+    'continent': 'Europe',
+    'island': 'kinda'},
+    {'name': 'Canada',
+    'continent': 'North America',
+    'island': 'nope'},
+    {'name': 'Guyana',
+    'continent': 'South America',
+    'island': 'nope'}
+]
